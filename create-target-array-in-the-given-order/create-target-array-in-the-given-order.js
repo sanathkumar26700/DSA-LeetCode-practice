dfefbf7ea9d9277e23773f6ec4f4be56f484620a
sanathkumar26700 <53991484+sanathkumar26700@users.noreply.github.com>
2021-11-19 14:05:47 +0530

    Time: 72 ms (73.56%), Space: 39 MB (30.89%) - LeetHub

---
+++ create-target-array-in-the-given-order/create-target-array-in-the-given-order.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @param {number[]} index
+ * @return {number[]}
+ */
+var createTargetArray = function(nums, index) {
+    let target =  []
+    for(i = 0; i < nums.length; i++){
+      target.splice(index[i], 0, nums[i])
+    }
+    
+    return target
+};
